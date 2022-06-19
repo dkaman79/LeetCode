f0c8430de90b222eab2a191f0ceb13b0980f23d7
Dharani <73869964+dkaman79@users.noreply.github.com>
2022-06-19 18:10:25 +0530

    Time: 16 ms (71.36%), Space: 11 MB (24.45%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[j]+nums[i]== target)
-                    return{i, j};
+        vector<int> ans;
+        unordered_map<int, int> mpp;  //declared the hash map
+    //iterate over the array
+        for(int i = 0; i<nums.size(); i++){
+            //here we use hash table to FIND the remaiming value is in the map or not
+            if(mpp.find(target - nums[i]) != mpp.end()) {
+                ans.push_back(mpp[target-nums[i]]); //we return value of index from the map
+                ans.push_back(i);
+                return ans;
             }
+            //if we don't find the number, we put it in the map.
+            mpp[nums[i]] = i;
         }
-        return {};
+        return ans;
     }
 };
